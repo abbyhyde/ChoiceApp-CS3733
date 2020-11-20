fee6aa40c06c30983b2bb3d3d5d1f141cf8b7afd
Vanessa Cardaropoli <vcardaropoli@wpi.edu>
2020-11-20 18:32:02 -0500

    create choice lambda function

--- src/main/java/com/amazonaws/lambda/demo/CreateChoiceHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/CreateChoiceHandler.java
@@ -1,1 +1,85 @@
+package com.amazonaws.lambda.demo;
+
+import java.io.ByteArrayInputStream;
+import java.sql.Date;
+import java.util.ArrayList;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+
+import com.amazonaws.lambda.demo.db.ChoiceDAO;
+import com.amazonaws.lambda.demo.http.Alternative;
+import com.amazonaws.lambda.demo.http.CreateChoiceRequest;
+import com.amazonaws.lambda.demo.http.CreateChoiceResponse;
+import com.amazonaws.lambda.demo.http.Member;
+import com.amazonaws.lambda.demo.model.Choice;
+
+/**
+ * Create a new constant and store in S3 bucket.
+
+ * @author heineman
+ */
+public class CreateChoiceHandler implements RequestHandler<CreateChoiceRequest,CreateChoiceResponse> {
+
+	LambdaLogger logger;
+	
+	// To access S3 storage
+	//private AmazonS3 s3 = null;
+		
+	// Note: this works, but it would be better to move this to environment/configuration mechanisms
+	// which you don't have to do for this project.
+	// public static final String REAL_BUCKET = "constants/";
+	
+	/** Store into RDS.
+	 * 
+	 * @throws Exception 
+	 */
+	boolean createChoice(String choiceId, String desc, ArrayList<Alternative> alts, int numM) throws Exception { 
+		if (logger != null) { logger.log("in createConstant"); }
+		ChoiceDAO dao = new ChoiceDAO();
+		
+		// check if present
+		Choice exist = dao.getChoice(choiceId);
+		Choice choice = new Choice(choiceId, desc, alts, numM);
+		if (exist == null) {
+			return dao.addChoice(choice);
+		} else {
+			return false;
+		}
+	}
+	
+	/** Create S3 bucket
+	 * 
+	 * @throws Exception 
+	 */
+	
+	
+	@Override 
+	public CreateChoiceResponse handleRequest(CreateChoiceRequest req, Context context)  {
+		logger = context.getLogger();
+		logger.log(req.toString());
+
+		CreateChoiceResponse response;
+		try {
+			if (createChoice(req.choiceId, req.description, req.alternatives,req.numMembers)) {
+				response = new CreateChoiceResponse(req.description);
+			} else {
+				response = new CreateChoiceResponse(req.description, 422);
+			}
+		} catch (Exception e) {
+			response = new CreateChoiceResponse("Unable to create choice: " + req.description + "(" + e.getMessage() + ")", 400);
+		}
+
+		return response;
+	}
+}
 
--- src/main/java/com/amazonaws/lambda/demo/http/CreateChoiceRequest.java
+++ src/main/java/com/amazonaws/lambda/demo/http/CreateChoiceRequest.java
@@ -1,1 +1,40 @@
+package com.amazonaws.lambda.demo.http;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class CreateChoiceRequest {
+	public String choiceId;
+	public String description;
+	public ArrayList<Alternative> alternatives;
+	public ArrayList<Member> members;
+	public int numMembers;
+	public boolean isCompleted;
+	public Date dateCompleted;
+	public Alternative altChosen;
+	
+	public CreateChoiceRequest() {}
+	
+	public CreateChoiceRequest(String desc, ArrayList<Alternative> alts, int numM) {
+		choiceId = UUID.randomUUID().toString();
+		this.description = desc;
+		this.alternatives = alts;
+		this.members = new ArrayList<>();
+		this.numMembers = numM;
+		this.isCompleted = false;
+		this.dateCompleted = null;
+		this.altChosen = null;
+	}
+	
+	public String toString() {
+		return "CreateChoice(" + choiceId + "," + description + ")";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
