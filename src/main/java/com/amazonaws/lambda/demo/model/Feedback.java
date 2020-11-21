4359ae444a1aa7be22e0bfa5bb5abfcc2455b76c
Vanessa Cardaropoli <vcardaropoli@wpi.edu>
2020-11-20 19:06:23 -0500

    made the feedback class

--- src/main/java/com/amazonaws/lambda/demo/model/Feedback.java
+++ src/main/java/com/amazonaws/lambda/demo/model/Feedback.java
@@ -1,1 +1,32 @@
+package com.amazonaws.lambda.demo.model;
 
+import java.sql.Date;
+
+public class Feedback{
+	
+	Date timeMade;
+	Member member;
+	String contents;
+	
+	public Feedback(Member m, String c) {
+		this.member = m;
+		this.contents = c;
+		this.timeMade = new Date(System.currentTimeMillis());
+	}
+	
+	public boolean equals(Object o) {
+		if (o == null) { return false; }
+		
+		if (o instanceof Feedback) {
+			Feedback other = (Feedback) o;
+			return (contents.equals(other.contents)) && (member.equals(other.member));
+		}
+		
+		return false;  // not a Feedback
+	}
+	
+	public String toString() {
+		return "Feedback(" + member.name + ": " + contents + ")";
+	}
+
+}
