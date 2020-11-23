46cd33c16fbf3fa6eadf46dd8d9e8f5a977940d2
Vanessa Cardaropoli <64789837+vncardaropoli@users.noreply.github.com>
2020-11-23 17:11:29 -0500

    Update add.js

--- js/add.js
+++ js/add.js
@@ -2,18 +2,27 @@
  * Respond to server JSON object.
  *
  */
-function processAddResponse(choiceID, name, pass) {
+function processAddResponse(choiceID, name, pass, result) {
   // Can grab any DIV or SPAN HTML element and can then manipulate its
   // contents dynamically via javascript
   console.log("result:" + result);
   var js = JSON.parse(result);
 
-  var computation = js["result"];
+
+  var choiceId = js["choiceId"];
+  var description = js["description];
+  //var alternatives
+  //var members
+  var maxNumMembers = js["maxNumMembers"];
+  var isCompleted = js["isCompleted"];
+  
   var status      = js["statusCode"];
   
   if (status == 200) {
     // Update computation result
-    document.addForm.result.value = computation
+    //document.addForm.result.value = computation
+    
+    //this is where you output all of the values from the variables above
   } else {
     var msg = js["error"];
     document.addForm.result.value = "error:" + msg
