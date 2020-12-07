8bd28035729bc2c44bf8ecb6a0681cde1f09a1f0
Aislin <ajhanscom@wpi.edu>
2020-12-07 15:29:33 -0500

    starting html/js for iteration 3

--- js/get_choiceList.js
+++ js/get_choiceList.js
@@ -33,14 +33,24 @@ function processListResponse(result) {
   // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
   var js = JSON.parse(result);
   var choiceList = document.getElementById("choices");
+  var choiceID = document.getElementById("choiceID");
+  var description = document.getElementById("description");
+  var creation = document.getElementById("createdDate");
+  var completed = document.getElementById("completed")
   var list = js["choices"];
 
   var output = "";
+  var choicesID = "";
+  var descripts = "";
+  var created = "";
+  var complete = "";
+
   for (var i = 0; i < list.length; i++) {
     var choiceJson = list[i];
     console.log(choiceJson);
     
     var choiceId = choiceJson["choiceId"];   		  //choice ID
+    var desc = choiceJson["description"];
     var dateCreated = choiceJson["dateCreated"];	  // creation date
 	if (choiceJson["isCompleted"]) {
 		var isCompleted = "completed";
@@ -48,11 +58,22 @@ function processListResponse(result) {
 		var isCompleted = "not completed";
 	}
 	
-    output = output + "<div id=\"choice" + choiceId + "\"><b>" + choiceId + "</b>   " + dateCreated + "   "  + isCompleted + "<br></div>";
-    
+	choicesID = choicesID + choiceId + "<br>"; 
+	
+	
+/*    //output = output + "<div id=\"choiceID" + choiceId + "\"><b>" + "<div id=\"description" + desc + "\"></b>" + "<div id=\"createdDate" + dateCreated + "\"></b>" + "<div id=\"completed" + isCompleted + "\"></b>" + "<br></div>";
+    choicesID =  choicesID + "<div id=\"choiceID" + choiceId + "\">" + "<br></div>";
+	descripts = descripts + "<div id=\"description" + desc + "\">" + "<br></div>";
+	created = created + "<div id=\"createdDate" + dateCreated + "\">" + "<br></div>";
+	complete = complete + "<div id=\"completed" + isCompleted + "\">" + "<br></div>";*/
+	
   }
 
   // Update computation result
-  choiceList.innerHTML = output;
+  //choiceList.innerHTML = output;
+	choiceID.innerHTML = choicesID;
+	description.innerHTML = descripts;
+	creation.innerHTML = created;
+	completed.innerHTML = complete;
 }
 
